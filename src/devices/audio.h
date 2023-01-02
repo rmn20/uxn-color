d94a84bc936e4a2c41338c386a33ba8fb4cc5b43
Andrew Alderwick <andrew@alderwick.co.uk>
2023-01-02 15:01:55 +0000

    Port Audio to new devices scheme.

--- src/devices/audio.h
+++ src/devices/audio.h
@@ -15,25 +15,8 @@ typedef signed int Sint32;
 #define SAMPLE_FREQUENCY 44100
 #define POLYPHONY 4
 
-#define DEVPEEK16(o, x) \
-	{ \
-		(o) = (d->dat[(x)] << 8) + d->dat[(x) + 1]; \
-	}
-#define DEVPOKE16(x, y) \
-	{ \
-		d->dat[(x)] = (y) >> 8; \
-		d->dat[(x) + 1] = (y); \
-	}
-
-typedef struct Device {
-	struct Uxn *u;
-	Uint8 dat[16];
-	Uint8 (*dei)(struct Device *d, Uint8);
-	void (*deo)(struct Device *d, Uint8);
-} Device;
-
 Uint8 audio_get_vu(int instance);
 Uint16 audio_get_position(int instance);
 int audio_render(int instance, Sint16 *sample, Sint16 *end);
-void audio_start(int instance, Device *d);
+void audio_start(int instance, Uint8 *d, Uxn *u);
 void audio_finished_handler(int instance);
