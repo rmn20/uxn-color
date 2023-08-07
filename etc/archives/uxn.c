e0bbf38af51ea4f2569efce703d00e224fdf5f8a
neauoire <aliceffekt@gmail.com>
2023-08-07 12:07:39 -0700

    (uxn.c) Reconnected the wires to the old core to be compatible with emu

--- etc/archives/uxn.c
+++ etc/archives/uxn.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "uxn.h"
 
 /*
@@ -11,22 +12,20 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE.
 */
 
-/*	a,b,c: general use. m2: byte/short mode flag.
-	tsp: macro temp stack ptr. ksp: "keep" mode copy of stack ptr. sp: ptr to stack ptr.
-	x,y: macro in params. o: macro out param. */
-
-#define HALT(c) { return uxn_halt(u, instr, (c), pc - 1); }
+#define HALT(c) { return emu_halt(u, instr, (c), pc - 1); }
 #define JUMP(x) { if(m2) pc = (x); else pc += (Sint8)(x); }
 #define PUSH8(x) { if(s->ptr == 0xff) HALT(2) s->dat[s->ptr++] = (x); }
-#define PUSH16(x) { if((tsp = s->ptr) >= 0xfe) HALT(2) t = (x); POKE16(&s->dat[tsp], t); s->ptr = tsp + 2; }
+#define PUSH16(x) { if((tsp = s->ptr) >= 0xfe) HALT(2) t = (x); POKE2(&s->dat[tsp], t); s->ptr = tsp + 2; }
 #define PUSH(x) { if(m2) { PUSH16(x) } else { PUSH8(x) } }
 #define POP8(o) { if(*sp == 0x00) HALT(1) o = s->dat[--*sp]; }
-#define POP16(o) { if((tsp = *sp) <= 0x01) HALT(1) o = PEEK16(&s->dat[tsp - 2]); *sp = tsp - 2; }
+#define POP16(o) { if((tsp = *sp) <= 0x01) HALT(1) o = PEEK2(&s->dat[tsp - 2]); *sp = tsp - 2; }
 #define POP(o) { if(m2) { POP16(o) } else { POP8(o) } }
-#define POKE(x, y) { if(m2) { t = (y); POKE16(u->ram + x, t) } else { u->ram[(x)] = (y); } }
-#define PEEK(o, x) { if(m2) { o = PEEK16(u->ram + x); } else o = u->ram[(x)]; }
-#define DEVR(o, x) { o = u->dei(u, x); if(m2) o = (o << 8) | u->dei(u, (x) + 1); }
-#define DEVW(x, y) { if(m2) { u->deo(u, (x), (y) >> 8); u->deo(u, (x) + 1, (y)); } else { u->deo(u, x, (y)); } }
+#define POKE(x, y) { if(m2) { t = (y); POKE2(u->ram + x, t) } else { u->ram[(x)] = (y); } }
+#define PEEK(o, x) { if(m2) { o = PEEK2(u->ram + x); } else o = u->ram[(x)]; }
+#define DEI(port) {  }
+#define DEO(port, value) { u->dev[(port)] = (value); if((deo_mask[(port) >> 4] >> ((port) & 0xf)) & 0x1) emu_deo(u, (port)); }
+#define DEVR(dest, port) { if(m2) { dest = (emu_dei(u, (port)) << 8) + emu_dei(u, (port+1)); } else { dest = emu_dei(u, (port)); } }
+#define DEVW(port, value) { if(m2) { DEO((port),(value >> 8)) DEO((port+1),(value)) } else { DEO((port),(value)) } }
 
 int
 uxn_eval(Uxn *u, Uint16 pc)
@@ -37,11 +36,12 @@ uxn_eval(Uxn *u, Uint16 pc)
 	if(!pc || u->dev[0x0f]) return 0;
 	for(;;) {
 		instr = u->ram[pc++];
+		/* printf("%02x\n", instr); */
 		opcode = instr & 0x1f;
 		/* Short Mode */
 		m2 = instr & 0x20;
 		/* Return Mode */
-		s = instr & 0x40 ? u->rst : u->wst;
+		s = instr & 0x40 ? &u->rst : &u->wst;
 		/* Keep Mode */
 		if(instr & 0x80) { ksp = s->ptr; sp = &ksp; }
 		else sp = &s->ptr;
@@ -50,12 +50,12 @@ uxn_eval(Uxn *u, Uint16 pc)
 		/* Immediate */
 		case -0x0: /* BRK */ return 1;
 		case -0x1: /* JCI */ POP8(b) if(!b) { pc += 2; break; } /* else fallthrough */
-		case -0x2: /* JMI */ pc += PEEK16(u->ram + pc) + 2; break;
-		case -0x3: /* JSI */ s = u->rst; PUSH16(pc + 2) pc += PEEK16(u->ram + pc) + 2; break;
+		case -0x2: /* JMI */ pc += PEEK2(u->ram + pc) + 2; break;
+		case -0x3: /* JSI */ s = &u->rst; PUSH16(pc + 2) pc += PEEK2(u->ram + pc) + 2; break;
 		case -0x4: /* LIT */
 		case -0x6: /* LITr */ a = u->ram[pc++]; PUSH8(a) break;
 		case -0x5: /* LIT2 */
-		case -0x7: /* LIT2r */ PUSH16(PEEK16(u->ram + pc)) pc += 2; break;
+		case -0x7: /* LIT2r */ PUSH16(PEEK2(u->ram + pc)) pc += 2; break;
 		/* ALU */
 		case 0x01: /* INC */ POP(a) PUSH(a + 1) break;
 		case 0x02: /* POP */ POP(a) break;
@@ -70,8 +70,8 @@ uxn_eval(Uxn *u, Uint16 pc)
 		case 0x0b: /* LTH */ POP(a) POP(b) PUSH8(b < a) break;
 		case 0x0c: /* JMP */ POP(a) JUMP(a) break;
 		case 0x0d: /* JCN */ POP(a) POP8(b) if(b) JUMP(a) break;
-		case 0x0e: /* JSR */ POP(a) s = (instr & 0x40) ? u->wst : u->rst; PUSH16(pc) JUMP(a) break;
-		case 0x0f: /* STH */ POP(a) s = (instr & 0x40) ? u->wst : u->rst; PUSH(a) break;
+		case 0x0e: /* JSR */ POP(a) s = (instr & 0x40) ? &u->wst : &u->rst; PUSH16(pc) JUMP(a) break;
+		case 0x0f: /* STH */ POP(a) s = (instr & 0x40) ? &u->wst : &u->rst; PUSH(a) break;
 		case 0x10: /* LDZ */ POP8(a) PEEK(b, a) PUSH(b) break;
 		case 0x11: /* STZ */ POP8(a) POP(b) POKE(a, b) break;
 		case 0x12: /* LDR */ POP8(a) PEEK(b, pc + (Sint8)a) PUSH(b) break;
@@ -93,17 +93,12 @@ uxn_eval(Uxn *u, Uint16 pc)
 }
 
 int
-uxn_boot(Uxn *u, Uint8 *ram, Dei *dei, Deo *deo)
+uxn_boot(Uxn *u, Uint8 *ram)
 {
 	Uint32 i;
 	char *cptr = (char *)u;
 	for(i = 0; i < sizeof(*u); i++)
-		cptr[i] = 0x00;
-	u->wst = (Stack *)(ram + 0xf0000);
-	u->rst = (Stack *)(ram + 0xf0100);
-	u->dev = (Uint8 *)(ram + 0xf0200);
+		cptr[i] = 0;
 	u->ram = ram;
-	u->dei = dei;
-	u->deo = deo;
 	return 1;
 }
